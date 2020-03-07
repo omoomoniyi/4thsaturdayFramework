cb1836dd699a336fc14f04bf83fed224419c45f8
Alex Ekisowe <alexe@sankore.local>
2020-03-07 11:47:50 +0100

    first push

---
+++ src/test/java/com/w2a/testcase/JumiaTest.java
@@ -0,0 +1,15 @@
+package com.w2a.testcase;
+
+
+import org.testng.annotations.Test;
+
+import com.w2a.base.TestBase;
+
+public class JumiaTest extends TestBase {
+
+	@Test
+	public void trial() {
+		
+		
+	}
+}
