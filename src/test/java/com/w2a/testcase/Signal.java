c905a622980fd1c92f4ef693320f551a382951bd
Arizona1010 <starinze@yahoo.com>
2020-03-07 12:50:46 -0800

    Initial Commit

---
+++ src/test/java/com/w2a/testcase/Signal.java
@@ -0,0 +1,23 @@
+package com.w2a.testcase;
+
+public class Signal {
+	package com.w2a.testcase;
+
+	import org.openqa.selenium.By;
+	import org.testng.Assert;
+	import org.testng.annotations.Test;
+
+	import com.w2a.base.TestBase;
+
+	public class LoginTest extends TestBase {
+		
+		@Test
+		public void validLogin () throws InterruptedException {
+			
+			click("loginpage_XPATH");
+			
+			Thread.sleep(2000);
+			driver.findElement(By.id(OR.getProperty("email_ID"))).sendKeys(OR.getProperty("email_TEXT"));
+			driver.findElement(By.id(OR.getProperty("password_ID"))).sendKeys(OR.getProperty("password_TEXT"));		
+
+}
